cc1940d2a0c8cf30498560fb4689481e4e213628
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-01-17 15:35:29 +0200

    Add import examples and add scripts to package.json

---
+++ src/es6.js
@@ -0,0 +1,5 @@
+export default {
+    hello(message){
+        console.log('es6:' + message);
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('hello webpack');
+const common = require('./common.js');
+common.hello('hello');
+import es6 from './es6.js';
+es6.hello('hello');
